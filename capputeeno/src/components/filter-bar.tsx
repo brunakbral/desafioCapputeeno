4b74ef0337694ac88c0b1694dfd54ff717e80892
Bruna Cabral <bruninha_kbral@hotmail.com>
2025-01-31 16:11:32 -0300

    feat: created filter bar

---
+++ capputeeno/src/components/filter-bar.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import styled from "styled-components";
+import { FilterByType } from "./filter-by-type";
+import { FilterByPriority } from "./filter-by-priority";
+
+interface FilterBarProps {
+
+}
+
+const FilterContainer = styled.div`
+    display: flex;
+    width: 100%;
+    align-items: start;
+    justify-content: space-between;
+    padding-left: 160px;
+
+
+`
+
+export function FilterBar(props:FilterBarProps){
+    return (
+        <FilterContainer>
+            <FilterByType/>
+            <FilterByPriority/>
+        </FilterContainer>
+    )
+}
---
+++ capputeeno/src/types/filter-types.ts
@@ -0,0 +1,5 @@
+export enum FilterType {
+    ALL = 'ALL',
+    SHIRT = 'SHIRT',
+    MUG = 'MUG'
+}
