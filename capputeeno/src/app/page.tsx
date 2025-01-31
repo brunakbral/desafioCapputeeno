7998247ad2fa01a98692cc13bd0ef423012cbee7
Bruna Cabral <bruninha_kbral@hotmail.com>
2025-01-31 16:12:31 -0300

    feat: intial products fetch

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,9 +1,19 @@
+"use client"
+
+import { FilterBar } from "@/components/filter-bar";
 import styles from "./page.module.css";
+import { ProductsList } from "@/components/products-list";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Home() {
+const client = new QueryClient ();
+
   return (
-    <div className={styles.page}>
-     
-    </div>
+    <QueryClientProvider client = {client}>
+      <main className={styles.page}>
+        <FilterBar />
+        <ProductsList />
+      </main>
+    </QueryClientProvider>
   );
 }
---
+++ capputeeno/src/hooks/useProducts.tsx
@@ -0,0 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosPromise } from "axios";
+
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+
+interface Product {
+  id: string;
+  name: string;
+  price_in_cents: number;
+}
+
+interface ProductsFetchResponse {
+  data: {
+    allProducts: Product[];
+  };
+}
+
+const fetcher = (): AxiosPromise<ProductsFetchResponse> => {
+  return axios.post(API_URL, {
+    query: `query {
+      allProducts {
+        id
+        name
+        price_in_cents
+        image_url
+      }
+    }`,
+  });
+};
+
+export function useProducts() {
+  const { data } = useQuery({
+    queryFn: fetcher, 
+    queryKey: ["products"],
+  });
+
+  return {
+    data: data?.data?.data?.allProducts,
+  };
+}
